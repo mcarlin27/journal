54524ada5354407e17226be3a043088a818cdf8c
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-06-27 14:49:32 -0700

    refactor weather-interface.js to allow for asynchronous code.

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -5,8 +5,13 @@ $(document).ready(function() {
     var city = $('#location').val();
     $('#location').val("");
     $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+    .then(function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%")
+
+      .fail(function(error) {
+        $('.showWeather').text(error.responseJSON.message);
+      });
     });
   });
 });
